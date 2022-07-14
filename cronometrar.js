1811cad022c553c98a37c700f6a9ad6cf1634c5b
Débora <d.saioron@hotmail.com>
2022-07-14 18:49:23 -0300

    mudanças no js

--- cronometrar.js
+++ cronometrar.js
@@ -1,13 +1,14 @@
 var segundos = 0;
 var minutos = 0;
 var horas = 0;
+var inicio = 0;
 
 function iniciar() {
-    setInterval(contador, 1000)
+     inicio = setInterval(contador, 1000)
 }
 
 function pausar () {
-  
+    clearInterval(inicio)
 }
 
 function reiniciar() {
@@ -16,4 +17,12 @@ function reiniciar() {
 
 function contador(){
     segundos++
+    if(segundos == 59) {
+       minutos++
+       segundos = 0
+    }
+    if(minutos == 59) {
+      horas ++
+    }
+    document.getElementById('contador').innerText = horas + ':' + minutos + ':' + segundos
 }
