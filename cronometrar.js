ffeab9b50afbd9fd1ae57871b5cecba9c3ddf686
Débora <d.saioron@hotmail.com>
2022-07-13 21:14:46 -0300

    criando funções

--- cronometrar.js
+++ cronometrar.js
@@ -1,3 +1,19 @@
 var segundos = 0;
 var minutos = 0;
 var horas = 0;
+
+function iniciar() {
+    setInterval(contador, 1000)
+}
+
+function pausar () {
+  
+}
+
+function reiniciar() {
+    
+}
+
+function contador(){
+    segundos++
+}
